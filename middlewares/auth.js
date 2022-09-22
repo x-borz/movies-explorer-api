1737da50c4c423b482f3f65a7fbc2a9497014b20
x-borz <KabanovRE@mosreg.ru>
2022-09-22 20:48:33 +0300

    fix

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const { JWT_SECRET } = require('../utils/constants');
 const AuthError = require('../errors/auth-error');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const { authorization = '' } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new AuthError('Необходима авторизация'));
