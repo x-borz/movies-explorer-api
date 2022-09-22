96bad934dd406b354570bbcf7ccd3169481b0ec8
x-borz <KabanovRE@mosreg.ru>
2022-09-22 16:44:42 +0300

    feat. Добавить логирование запросов и ошибок сервера в json-формате

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { PORT } = require('./utils/constants');
 const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
@@ -12,8 +13,11 @@ mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(requestLogger);
 
 app.use('/', require('./routes'));
+
+app.use(errorLogger);
 
 app.use(errorHandler);
 
