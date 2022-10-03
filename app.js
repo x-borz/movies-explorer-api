f8d40a2d8e20a5b41d9110ae772bbece479122e8
x-borz <KabanovRE@mosreg.ru>
2022-10-03 12:56:15 +0300

    feat. CORS-заголовки

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-  origin: ['https://api.xborz.diploma.nomoredomains.club', 'http://api.xborz.diploma.nomoredomains.club'],
+  origin: '*',
   methods: 'GET,POST,DELETE,OPTIONS,PATCH',
 }));
 
